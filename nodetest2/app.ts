00003eb7953f281fd7549073fa8aafecbb6d96cd
MakinoAlan <yangmuzhi1993@gmail.com>
2015-09-30 13:06:34 -0700

    handle conflict test

--- nodetest2/app.ts
+++ nodetest2/app.ts
@@ -52,7 +52,7 @@ this.app.use('/users', users);
 // catch 404 and forward to error handler
 this.app.use(function(req, res, next) {
   var err = new Error('Not Found');
-  err.status = 404;
+  err.status = 100;
   next(err);
 });
 
